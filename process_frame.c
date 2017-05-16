ea846a74f8cd9d9dba2a7ea7c027e985de68dd1d
oscar <oscar@oscar.(none)>
2017-05-16 20:13:49 +0200

    working color segmentation of stones with ycbcr

--- process_frame.c
+++ process_frame.c
@@ -6,7 +6,7 @@
 /*! @file process_frame.c
  * @brief Contains the actual algorithm and calculations.
  */
- 
+
 /* Definitions specific to this application. Also includes the Oscar main header file. */
 #include "template.h"
 #include <string.h>
@@ -36,69 +36,74 @@ void Erode_3x3(int InIndex, int OutIndex);
 void Dilate_3x3(int InIndex, int OutIndex);
 void DetectRegions();
 void DrawBoundingBoxes();
+void ChangeDetection(void);
 
-
-void ResetProcess()
-{
+void ResetProcess() {
 	//called when "reset" button is pressed
-	if(ManualThreshold == false)
+	if (ManualThreshold == false)
 		ManualThreshold = true;
 	else
 		ManualThreshold = false;
 }
 
-
 void ProcessFrame() {
 	//initialize counters
-	if(data.ipc.state.nStepCounter == 1) {
+	if (data.ipc.state.nStepCounter == 1) {
 		ManualThreshold = false;
 	} else {
+#if NUM_COLORS == 3 //if color is used, the image threshold is stored in index1
+
+		ChangeDetection();
+		//Erode_3x3(INDEX1, INDEX0);
+		//Dilate_3x3(INDEX0, THRESHOLD);
+
+#elif NUM_COLORS == 1 //if the image is in BW, use Otsu's Method to determine the threshold
+
 		unsigned char Threshold = OtsuThreshold(SENSORIMG);
-
 		Binarize(Threshold);
-
 		Erode_3x3(THRESHOLD, INDEX0);
 		Dilate_3x3(INDEX0, THRESHOLD);
 
-		DetectRegions();
+#endif
 
+		DetectRegions();
 		DrawBoundingBoxes();
 
-		if(ManualThreshold) {
+		if (ManualThreshold) {
 			char Text[] = "manual threshold";
 			DrawString(20, 20, strlen(Text), SMALL, CYAN, Text);
 		} else {
 			char Text[] = " Otsu's threshold";
 			DrawString(20, 20, strlen(Text), SMALL, CYAN, Text);
 		}
+
 	}
 }
 
-
-
+#if NUM_COLORS == 1
 void Binarize(unsigned char threshold) {
 	int r, c;
 	//set result buffer to zero
 	memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
 
 	//loop over the rows
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
+	for (r = Border * nc; r < (nr - Border) * nc; r += nc) {
 		//loop over the columns
-		for(c = Border; c < (nc-Border); c++) {
+		for (c = Border; c < (nc - Border); c++) {
 			//manual threshold?
-			if(ManualThreshold) {
-				if(data.u8TempImage[SENSORIMG][r+c] < data.ipc.state.nThreshold) {
-					data.u8TempImage[THRESHOLD][r+c] = 255;
+			if (ManualThreshold) {
+				if (data.u8TempImage[SENSORIMG][r + c]
+						< data.ipc.state.nThreshold) {
+					data.u8TempImage[THRESHOLD][r + c] = 255;
 				}
 			} else {
-				if(data.u8TempImage[SENSORIMG][r+c] < threshold) {
-					data.u8TempImage[THRESHOLD][r+c] = 255;
+				if (data.u8TempImage[SENSORIMG][r + c] < threshold) {
+					data.u8TempImage[THRESHOLD][r + c] = 255;
 				}
 			}
 		}
 	}
 }
-
 
 unsigned char OtsuThreshold(int InIndex) {
 	//first part: extract gray value histogram
@@ -108,32 +113,32 @@ unsigned char OtsuThreshold(int InIndex) {
 	float best;
 	memset(Hist, 0, sizeof(Hist));
 
-	for(i1 = 0; i1 < nr*nc; i1++) {
+	for (i1 = 0; i1 < nr * nc; i1++) {
 		Hist[p[i1]] += 1;
 	}
 	//second part: determine threshold according to Otsu's method
 	best = 0;
 	best_i = 0;
-	for(K = 0; K < 255; K++)	{
+	for (K = 0; K < 255; K++) {
 		//the class accumulators
 		float w0 = 0, mu0 = 0, w1 = 0, mu1 = 0;
 		float bestloc;
 
 		//class 0 and 1 probabilities and averages
-		for(i1 = 0; i1 <= K; i1++)	{
+		for (i1 = 0; i1 <= K; i1++) {
 			w0 += Hist[i1];
-			mu0 += (Hist[i1]*i1);
+			mu0 += (Hist[i1] * i1);
 		}
-		for(; i1 <= 255; i1++) {
+		for (; i1 <= 255; i1++) {
 			w1 += Hist[i1];
-			mu1 += (Hist[i1]*i1);
+			mu1 += (Hist[i1] * i1);
 		}
 		//do normalization of average values
 		mu0 /= w0;
 		mu1 /= w1;
 
-		bestloc = w0*w1*(mu0-mu1)*(mu0-mu1);
-		if(bestloc > best) {
+		bestloc = w0 * w1 * (mu0 - mu1) * (mu0 - mu1);
+		if (bestloc > best) {
 			best = bestloc;
 			best_i = K;
 			//OscLog(INFO, "%d %d %d %d %d\n", i1, w0, w1, mu0, mu1);
@@ -143,41 +148,39 @@ unsigned char OtsuThreshold(int InIndex) {
 	return (unsigned char) best_i;
 }
 
-void Erode_3x3(int InIndex, int OutIndex)
-{
+#endif
+
+void Erode_3x3(int InIndex, int OutIndex) {
 	int c, r;
 
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		for(c = Border; c < (nc-Border); c++) {
-			unsigned char* p = &data.u8TempImage[InIndex][r+c];
-			data.u8TempImage[OutIndex][r+c] = *(p-nc-1) & *(p-nc) & *(p-nc+1) &
-											   *(p-1)    & *p      & *(p+1)    &
-											   *(p+nc-1) & *(p+nc) & *(p+nc+1);
+	for (r = Border * nc; r < (nr - Border) * nc; r += nc) {
+		for (c = Border; c < (nc - Border); c++) {
+			unsigned char* p = &data.u8TempImage[InIndex][r + c];
+			data.u8TempImage[OutIndex][r + c] = *(p - nc - 1) & *(p - nc)
+					& *(p - nc + 1) & *(p - 1) & *p & *(p + 1) & *(p + nc - 1)
+					& *(p + nc) & *(p + nc + 1);
 		}
 	}
 }
 
-void Dilate_3x3(int InIndex, int OutIndex)
-{
+void Dilate_3x3(int InIndex, int OutIndex) {
 	int c, r;
 
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		for(c = Border; c < (nc-Border); c++) {
-			unsigned char* p = &data.u8TempImage[InIndex][r+c];
-			data.u8TempImage[OutIndex][r+c] = *(p-nc-1) | *(p-nc) | *(p-nc+1) |
-											        *(p-1)    | *p      | *(p+1)    |
-											        *(p+nc-1) | *(p+nc) | *(p+nc+1);
+	for (r = Border * nc; r < (nr - Border) * nc; r += nc) {
+		for (c = Border; c < (nc - Border); c++) {
+			unsigned char* p = &data.u8TempImage[InIndex][r + c];
+			data.u8TempImage[OutIndex][r + c] = *(p - nc - 1) | *(p - nc)
+					| *(p - nc + 1) | *(p - 1) | *p | *(p + 1) | *(p + nc - 1)
+					| *(p + nc) | *(p + nc + 1);
 		}
 	}
 }
-
 
 void DetectRegions() {
 	struct OSC_PICTURE Pic;
 	int i;
-
 	//set pixel value to 1 in INDEX0 because the image MUST be binary (i.e. values of 0 and 1)
-	for(i = 0; i < IMG_SIZE; i++) {
+	for (i = 0; i < IMG_SIZE; i++) {
 		data.u8TempImage[INDEX0][i] = data.u8TempImage[THRESHOLD][i] ? 1 : 0;
 	}
 
@@ -188,25 +191,89 @@ void DetectRegions() {
 	Pic.type = OSC_PICTURE_BINARY;
 
 	//now do region labeling and feature extraction
-	OscVisLabelBinary( &Pic, &ImgRegions);
-	OscVisGetRegionProperties( &ImgRegions);
+	OscVisLabelBinary(&Pic, &ImgRegions);
+	OscVisGetRegionProperties(&ImgRegions);
+#if NUM_COLORS == 3
+
+#endif
 }
 
-
 void DrawBoundingBoxes() {
-	uint16 o;
-	for(o = 0; o < ImgRegions.noOfObjects; o++) {
-		if(ImgRegions.objects[o].area > MinArea) {
-			DrawBoundingBox(ImgRegions.objects[o].bboxLeft, ImgRegions.objects[o].bboxTop,
-							ImgRegions.objects[o].bboxRight, ImgRegions.objects[o].bboxBottom, false, GREEN);
+uint16 o;
+for (o = 0; o < ImgRegions.noOfObjects; o++) {
+	if (ImgRegions.objects[o].area > MinArea) {
+		DrawBoundingBox(ImgRegions.objects[o].bboxLeft,
+				ImgRegions.objects[o].bboxTop, ImgRegions.objects[o].bboxRight,
+				ImgRegions.objects[o].bboxBottom, false, GREEN);
 
-			DrawLine(ImgRegions.objects[o].centroidX-SizeCross, ImgRegions.objects[o].centroidY,
-					 ImgRegions.objects[o].centroidX+SizeCross, ImgRegions.objects[o].centroidY, RED);
-			DrawLine(ImgRegions.objects[o].centroidX, ImgRegions.objects[o].centroidY-SizeCross,
-								 ImgRegions.objects[o].centroidX, ImgRegions.objects[o].centroidY+SizeCross, RED);
+		DrawLine(ImgRegions.objects[o].centroidX - SizeCross,
+				ImgRegions.objects[o].centroidY,
+				ImgRegions.objects[o].centroidX + SizeCross,
+				ImgRegions.objects[o].centroidY, RED);
+		DrawLine(ImgRegions.objects[o].centroidX,
+				ImgRegions.objects[o].centroidY - SizeCross,
+				ImgRegions.objects[o].centroidX,
+				ImgRegions.objects[o].centroidY + SizeCross, RED);
 
+	}
+}
+}
+
+void ChangeDetection() {
+#define NumFgrCol 2
+
+uint8 FrgCol[NumFgrCol][2] = { { 128 - 12, 128 + 38 }, { 128 + 24, 128 - 17 } };
+int r, c, frg, p;
+
+memset(data.u8TempImage[INDEX0], 0, IMG_SIZE);
+memset(data.u8TempImage[BACKGROUND], 0, IMG_SIZE);
+memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
+
+//loop over the rows
+for (r = 0; r < nr * nc; r += nc) {
+//loop over the columns
+	for (c = 0; c < nc; c++) {
+		//convert rgb to ycbcr
+		//get rgb values (order is actually bgr!)
+		float B_ = data.u8TempImage[SENSORIMG][(r + c) * NUM_COLORS + 0];
+		float G_ = data.u8TempImage[SENSORIMG][(r + c) * NUM_COLORS + 1];
+		float R_ = data.u8TempImage[SENSORIMG][(r + c) * NUM_COLORS + 2];
+		uint8 Y_ = (uint8) (0 + 0.299 * R_ + 0.587 * G_ + 0.114 * B_);
+		uint8 Cb_ = (uint8) (128 - 0.169 * R_ - 0.331 * G_ + 0.500 * B_);
+		uint8 Cr_ = (uint8) (128 + 0.500 * R_ - 0.419 * G_ - 0.081 * B_);
+		//we write result to ImIndex
+		data.u8TempImage[THRESHOLD][(r + c) * NUM_COLORS + 0] = Y_;
+		data.u8TempImage[THRESHOLD][(r + c) * NUM_COLORS + 1] = Cb_;
+		data.u8TempImage[THRESHOLD][(r + c) * NUM_COLORS + 2] = Cr_;
+
+		//loop over the different Frg colors and find smallest difference
+		int MinDif = 1 << 30;
+		int MinInd = 0;
+		for (frg = 0; frg < NumFgrCol; frg++) {
+			int Dif = 0;
+			//loop over the color planes (Cb,Cr) and sum up the difference
+			for (p = 0; p < NUM_CHROM; p++) {
+				Dif += abs(
+						(int) data.u8TempImage[THRESHOLD][(r + c) * NUM_COLORS
+								+ p + 1] - (int) FrgCol[frg][p]);
+			}
+			if (Dif < MinDif) {
+				MinDif = Dif;
+				MinInd = frg;
+			}
+		}
+		//if the difference is smaller than threshold value
+		if (MinDif < data.ipc.state.nThreshold) {
+			//set pixel value to 255 in THRESHOLD image for further processing
+			//(we use only the first third of the image buffer)
+			data.u8TempImage[INDEX1][(r + c)] = 255;
+			//set pixel value to Frg color in BACKGROUND image for visualization
+			for (p = 0; p < NUM_CHROM; p++) {
+				data.u8TempImage[BACKGROUND][(r + c) * NUM_COLORS + p] =
+						FrgCol[MinInd][p];
+			}
 		}
 	}
 }
-
+}
 
